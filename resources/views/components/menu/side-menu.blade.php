c276e4d7eaa0f1cd33224b2a1d634014d2672f14
“SUNDAR-Office” <aaranoffice@gmail.com>
2024-04-10 13:47:50 +0530

    #1 update menu

--- resources/views/components/menu/side-menu.blade.php
+++ resources/views/components/menu/side-menu.blade.php
@@ -25,11 +25,22 @@
         <ul class="flex flex-col py-6 space-y-1"
             x-data="{selected:null}">
 
-            <x-menu.sub.offset/>
+            <x-menu.sub.audit/>
+            <x-menu.sub.task/>
+            <x-menu.sub.admin/>
+            <x-menu.sub.accounts/>
+
             <x-menu.sub.master/>
             <x-menu.sub.common/>
-            <x-menu.sub.logout/>
 
+            <x-menu.sub.order/>
+            <x-menu.sub.entries/>
+            <x-menu.sub.offset/>
+
+            <x-menu.sub.utilities/>
+            <x-menu.sub.magalir/>
+
+            <x-menu.sub.logout/>
         </ul>
     </div>
 </nav>
