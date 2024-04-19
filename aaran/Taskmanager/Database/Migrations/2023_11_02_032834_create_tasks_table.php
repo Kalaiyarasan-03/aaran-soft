ff34396167fdbe02340b92967f3178bbb8f78b32
“SUNDAR-Office” <aaranoffice@gmail.com>
2024-04-19 11:44:23 +0530

    #161 working on masterGst

--- aaran/Taskmanager/Database/Migrations/2023_11_02_032834_create_tasks_table.php
+++ aaran/Taskmanager/Database/Migrations/2023_11_02_032834_create_tasks_table.php
@@ -10,14 +10,14 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('client_id')->references('id')->on('clients');
             $table->string('title');
             $table->longText('body');
+            $table->foreignId('client_id')->nullable();
             $table->string('channel', 3)->nullable();
-            $table->foreignId('company_id')->references('id')->on('companies');
             $table->foreignId('allocated')->references('id')->on('users');
-            $table->foreignId('user_id')->references('id')->on('users');
             $table->string('status', 3)->nullable();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('company_id')->references('id')->on('companies');
             $table->string('active_id', 3)->nullable();
             $table->timestamps();
         });
