84ddbf96e7fdcf2b9a5aeeb318e96e9e1c1f10e2
Gowtham <gowtham@aaran.org>
2024-04-23 10:18:48 +0530

    #198  rework on todo

--- resources/views/livewire/dashboard/index.blade.php
+++ resources/views/livewire/dashboard/index.blade.php
@@ -1,20 +1,17 @@
 <div>
     <x-slot name="header">Dashboard</x-slot>
     <div class="w-full h-screen">
-        <div class="flex flex-col sm:flex-row gap-3 py-5">
-
-            <div class="w-full">
+        <div class="flex justify-between gap-3 py-5 xl:pl-12 h-2/5 ">
+            <div class="w-1/4 ">
                 @if(Aaran\Aadmin\Src\Customise::hasTodoList())
                     <livewire:taskmanager.todos.index/>
                 @endif
 
             </div>
 
-            <div class="w-full">
                 @if(Aaran\Aadmin\Src\Customise::hasAttendance())
                     <livewire:attendance.attendance.index/>
                 @endif
-            </div>
 
         </div>
         <div class="fixed right-0 px-2 py-2 text-gray-400 bottom-0">
