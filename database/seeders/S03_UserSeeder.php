b3bb7b6a54c6970604756a8709c711abb04769e7
SUNDAR <aaransoftware@gmail.com>
2024-04-25 08:54:33 +0530

    #219 refactor user seeder

--- database/seeders/S03_UserSeeder.php
+++ database/seeders/S03_UserSeeder.php
@@ -23,16 +23,7 @@ class S03_UserSeeder extends Seeder
 
         User::create([
             'name' => 'Developer',
-            'email' => 'developer@aaran.com',
-            'password' => bcrypt('kalarani'),
-            'email_verified_at' => now(),
-            'remember_token' => Str::random(10),
-
-        ]);
-
-        User::create([
-            'name' => 'Jagadeesh',
-            'email' => 'Jagadeesh@aaran.org',
+            'email' => 'developer@aaran.org',
             'password' => bcrypt('123456789'),
             'email_verified_at' => now(),
             'remember_token' => Str::random(10),
@@ -40,9 +31,10 @@ class S03_UserSeeder extends Seeder
             'role_id' => '1'
         ]);
 
+
         User::create([
-            'name' => 'Divya',
-            'email' => 'divya@aaran.org',
+            'name' => 'Audit',
+            'email' => 'audit@aaran.org',
             'password' => bcrypt('123456789'),
             'email_verified_at' => now(),
             'remember_token' => Str::random(10),
@@ -50,54 +42,74 @@ class S03_UserSeeder extends Seeder
             'role_id' => '1'
         ]);
 
-        User::create([
-            'name' => 'Kalaiarasan',
-            'email' => 'kalaiarasan@aaran.org',
-            'password' => bcrypt('123456789'),
-            'email_verified_at' => now(),
-            'remember_token' => Str::random(10),
-            'tenant_id' => '1',
-            'role_id' => '1'
-        ]);
-
-        User::create([
-            'name' => 'Alagiri Sankar',
-            'email' => 'alagiri@aaran.org',
-            'password' => bcrypt('123456789'),
-            'email_verified_at' => now(),
-            'remember_token' => Str::random(10),
-            'tenant_id' => '1',
-            'role_id' => '1'
-        ]);
-
-        User::create([
-            'name' => 'kapinjal',
-            'email' => 'kapi@aaran.org',
-            'password' => bcrypt('123456789'),
-            'email_verified_at' => now(),
-            'remember_token' => Str::random(10),
-            'tenant_id' => '1',
-            'role_id' => '1'
-        ]);
-
-        User::create([
-            'name' => 'SATHISHKUMAR',
-            'email' => 'sathish@aaran.org',
-            'password' => bcrypt('123456789'),
-            'email_verified_at' => now(),
-            'remember_token' => Str::random(10),
-            'tenant_id' => '1',
-            'role_id' => '1'
-        ]);
-
-        User::create([
-            'name' => 'KALARANI',
-            'email' => 'kalarani@aaran.org',
-            'password' => bcrypt('123456789'),
-            'email_verified_at' => now(),
-            'remember_token' => Str::random(10),
-            'tenant_id' => '1',
-            'role_id' => '1'
-        ]);
+//        User::create([
+//            'name' => 'Jagadeesh',
+//            'email' => 'Jagadeesh@aaran.org',
+//            'password' => bcrypt('123456789'),
+//            'email_verified_at' => now(),
+//            'remember_token' => Str::random(10),
+//            'tenant_id' => '1',
+//            'role_id' => '1'
+//        ]);
+//
+//        User::create([
+//            'name' => 'Divya',
+//            'email' => 'divya@aaran.org',
+//            'password' => bcrypt('123456789'),
+//            'email_verified_at' => now(),
+//            'remember_token' => Str::random(10),
+//            'tenant_id' => '1',
+//            'role_id' => '1'
+//        ]);
+//
+//        User::create([
+//            'name' => 'Kalaiarasan',
+//            'email' => 'kalaiarasan@aaran.org',
+//            'password' => bcrypt('123456789'),
+//            'email_verified_at' => now(),
+//            'remember_token' => Str::random(10),
+//            'tenant_id' => '1',
+//            'role_id' => '1'
+//        ]);
+//
+//        User::create([
+//            'name' => 'Alagiri Sankar',
+//            'email' => 'alagiri@aaran.org',
+//            'password' => bcrypt('123456789'),
+//            'email_verified_at' => now(),
+//            'remember_token' => Str::random(10),
+//            'tenant_id' => '1',
+//            'role_id' => '1'
+//        ]);
+//
+//        User::create([
+//            'name' => 'kapinjal',
+//            'email' => 'kapi@aaran.org',
+//            'password' => bcrypt('123456789'),
+//            'email_verified_at' => now(),
+//            'remember_token' => Str::random(10),
+//            'tenant_id' => '1',
+//            'role_id' => '1'
+//        ]);
+//
+//        User::create([
+//            'name' => 'SATHISHKUMAR',
+//            'email' => 'sathish@aaran.org',
+//            'password' => bcrypt('123456789'),
+//            'email_verified_at' => now(),
+//            'remember_token' => Str::random(10),
+//            'tenant_id' => '1',
+//            'role_id' => '1'
+//        ]);
+//
+//        User::create([
+//            'name' => 'KALARANI',
+//            'email' => 'kalarani@aaran.org',
+//            'password' => bcrypt('123456789'),
+//            'email_verified_at' => now(),
+//            'remember_token' => Str::random(10),
+//            'tenant_id' => '1',
+//            'role_id' => '1'
+//        ]);
     }
 }
