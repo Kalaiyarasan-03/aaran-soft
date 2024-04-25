afeec6cd29fe5359c0ae19f6c33e68114d108bdd
kalai <kalaiararasan200229@gmail.com>
2024-04-25 16:56:40 +0530

    #223 refactor in new button for responsive view

--- resources/views/livewire/entries/sales/index.blade.php
+++ resources/views/livewire/entries/sales/index.blade.php
@@ -21,9 +21,6 @@
                 <x-table.header-text wire:click.prevent="sortBy('invoice_no')" center>Grand Total</x-table.header-text>
                 <x-table.header-text center>Print</x-table.header-text>
 
-                <x-table.header-text center>Grand Total</x-table.header-text>
-                <x-table.header-text center>print</x-table.header-text>
-
                 <x-table.header-action/>
             </x-slot>
 
