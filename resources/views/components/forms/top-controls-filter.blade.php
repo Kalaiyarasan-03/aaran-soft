f225aaf9d4c8b1073b27e1aad672c9f6b2a42586
kalai <kalaiararasan200229@gmail.com>
2024-04-27 12:55:35 +0530

    #235 rework on top_controls_blade and grand total alignment

--- resources/views/components/forms/top-controls-filter.blade.php
+++ resources/views/components/forms/top-controls-filter.blade.php
@@ -2,15 +2,15 @@
 'showFilters'=>false,
 ])
 
-<div class="md:flex md:justify-between md:pb-5">
-    <div class="w-full h-20 md:w-2/4 flex md:space-x-2">
+<div class="md:flex md:justify-between md:items-center">
+    <div class="w-full h-20 md:w-2/4 md:items-center flex md:space-x-2">
 
         <x-input.search-box/>
         <x-input.toggle-filter :show-filters="$showFilters"/>
 
     </div>
 
-    <div class="flex justify-between md:mb-5 md:space-x-2 md:flex md:items-center">
+    <div class="flex justify-between items-center  md:space-x-2 md:flex md:items-center">
         <x-forms.per-page/>
         <x-button.new/>
     </div>
